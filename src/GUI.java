04ac9ff799541405ee290d4742ae1ccaccb9416d
JohanStener <eaajojs@students.eaaa.dk>
2023-03-03 11:51:05 +0100

    Client

--- src/GUI.java
+++ src/GUI.java
@@ -1,5 +1,9 @@
 
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,8 +18,19 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.*;
 
 public class GUI extends Application {
+	String sentence;
+	String modifiedSentence;
+	BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+	Socket clientSocket= new Socket("localhost",6789);
+	DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+	BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	sentence = inFromUser.readLine();
+		outToServer.writeBytes(sentence + '\n');
+	modifiedSentence = inFromServer.readLine();
+		System.out.println("FROM SERVER: " + modifiedSentence);
+		clientSocket.close();
 
-	public static final int size = 20; 
+	public static final int size = 20;
 	public static final int scene_height = size * 20 + 100;
 	public static final int scene_width = size * 20 + 200;
 
