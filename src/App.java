35e25ecdcb0703044be4101d169a702b4bc57a0c
Oskar <eaaoska@students.eaaa.dk>
2023-03-06 16:01:57 +0100

    Send to server moved to GUI

--- src/App.java
+++ src/App.java
@@ -1,5 +1,3 @@
-
-
 import javafx.application.Application;
 
 import java.io.BufferedReader;
@@ -11,16 +9,7 @@ import java.net.Socket;
 public class App {
 
 	public static void main(String[] args) throws IOException {
-		String modifiedSentence;
-		BufferedReader fromGame = new BufferedReader(new InputStreamReader(System.in));
-		Socket clientSocket = new Socket("localhost",6780);
-		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		String sentence = fromGame.readLine();
-		outToServer.writeBytes(sentence + '\n');
-		modifiedSentence = inFromServer.readLine();
-		System.out.println("FROM SERVER: " + modifiedSentence);
-		clientSocket.close();
+
 		Application.launch(GUI.class);
 
 	}
--- src/GUI.java
+++ src/GUI.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +18,7 @@ import javafx.scene.image.*;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.*;
+
 import java.io.*;
 import java.net.*;
 
@@ -27,35 +29,35 @@ public class GUI extends Application {
 
 	public static Image image_floor;
 	public static Image image_wall;
-	public static Image hero_right,hero_left,hero_up,hero_down;
+	public static Image hero_right, hero_left, hero_up, hero_down;
 
 	public static Player me;
 	public static List<Player> players = new ArrayList<Player>();
 
 	private Label[][] fields;
 	private TextArea scoreList;
-	
-	private  String[] board = {    // 20x20
-			"wwwwwwwwwwwwwwwwwwww",
-			"w        ww        w",
-			"w w  w  www w  w  ww",
-			"w w  w   ww w  w  ww",
-			"w  w               w",
-			"w w w w w w w  w  ww",
-			"w w     www w  w  ww",
-			"w w     w w w  w  ww",
-			"w   w w  w  w  w   w",
-			"w     w  w  w  w   w",
-			"w ww ww        w  ww",
-			"w  w w    w    w  ww",
-			"w        ww w  w  ww",
-			"w         w w  w  ww",
-			"w        w     w  ww",
-			"w  w              ww",
-			"w  w www  w w  ww ww",
-			"w w      ww w     ww",
-			"w   w   ww  w      w",
-			"wwwwwwwwwwwwwwwwwwww"
+
+	private String[] board = {    // 20x20
+					"wwwwwwwwwwwwwwwwwwww",
+					"w        ww        w",
+					"w w  w  www w  w  ww",
+					"w w  w   ww w  w  ww",
+					"w  w               w",
+					"w w w w w w w  w  ww",
+					"w w     www w  w  ww",
+					"w w     w w w  w  ww",
+					"w   w w  w  w  w   w",
+					"w     w  w  w  w   w",
+					"w ww ww        w  ww",
+					"w  w w    w    w  ww",
+					"w        ww w  w  ww",
+					"w         w w  w  ww",
+					"w        w     w  ww",
+					"w  w              ww",
+					"w  w www  w w  ww ww",
+					"w w      ww w     ww",
+					"w   w   ww  w      w",
+					"wwwwwwwwwwwwwwwwwwww"
 	};
 
 	public GUI() throws IOException {
@@ -79,110 +81,148 @@ public class GUI extends Application {
 
 			Text mazeLabel = new Text("Maze:");
 			mazeLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-	
+
 			Text scoreLabel = new Text("Score:");
 			scoreLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
 			scoreList = new TextArea();
-			
+
 			GridPane boardGrid = new GridPane();
 
-			image_wall  = new Image(getClass().getResourceAsStream("Image/wall4.png"),size,size,false,false);
-			image_floor = new Image(getClass().getResourceAsStream("Image/floor1.png"),size,size,false,false);
+			image_wall = new Image(getClass().getResourceAsStream("Image/wall4.png"), size, size, false, false);
+			image_floor = new Image(getClass().getResourceAsStream("Image/floor1.png"), size, size, false, false);
 
-			hero_right  = new Image(getClass().getResourceAsStream("Image/heroRight.png"),size,size,false,false);
-			hero_left   = new Image(getClass().getResourceAsStream("Image/heroLeft.png"),size,size,false,false);
-			hero_up     = new Image(getClass().getResourceAsStream("Image/heroUp.png"),size,size,false,false);
-			hero_down   = new Image(getClass().getResourceAsStream("Image/heroDown.png"),size,size,false,false);
+			hero_right = new Image(getClass().getResourceAsStream("Image/heroRight.png"), size, size, false, false);
+			hero_left = new Image(getClass().getResourceAsStream("Image/heroLeft.png"), size, size, false, false);
+			hero_up = new Image(getClass().getResourceAsStream("Image/heroUp.png"), size, size, false, false);
+			hero_down = new Image(getClass().getResourceAsStream("Image/heroDown.png"), size, size, false, false);
 
 			fields = new Label[20][20];
-			for (int j=0; j<20; j++) {
-				for (int i=0; i<20; i++) {
+			for (int j = 0; j < 20; j++) {
+				for (int i = 0; i < 20; i++) {
 					switch (board[j].charAt(i)) {
-					case 'w':
-						fields[i][j] = new Label("", new ImageView(image_wall));
-						break;
-					case ' ':					
-						fields[i][j] = new Label("", new ImageView(image_floor));
-						break;
-					default: throw new Exception("Illegal field value: "+board[j].charAt(i) );
+						case 'w':
+							fields[i][j] = new Label("", new ImageView(image_wall));
+							break;
+						case ' ':
+							fields[i][j] = new Label("", new ImageView(image_floor));
+							break;
+						default:
+							throw new Exception("Illegal field value: " + board[j].charAt(i));
 					}
 					boardGrid.add(fields[i][j], i, j);
 				}
 			}
 			scoreList.setEditable(false);
-			
-			
-			grid.add(mazeLabel,  0, 0); 
-			grid.add(scoreLabel, 1, 0); 
-			grid.add(boardGrid,  0, 1);
-			grid.add(scoreList,  1, 1);
-						
-			Scene scene = new Scene(grid,scene_width,scene_height);
+
+
+			grid.add(mazeLabel, 0, 0);
+			grid.add(scoreLabel, 1, 0);
+			grid.add(boardGrid, 0, 1);
+			grid.add(scoreList, 1, 1);
+
+			Scene scene = new Scene(grid, scene_width, scene_height);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 
 			scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+				String keyPressed;
 				switch (event.getCode()) {
-				case UP:    playerMoved(0,-1,"up");    break;
-				case DOWN:  playerMoved(0,+1,"down");  break;
-				case LEFT:  playerMoved(-1,0,"left");  break;
-				case RIGHT: playerMoved(+1,0,"right"); break;
-				default: break;
+					case UP:
+						playerMoved(0, -1, "up");
+						keyPressed = "UP!";
+						try {
+							sendInputToServer(keyPressed);
+						} catch (IOException e) {
+							throw new RuntimeException(e);
+						}
+						break;
+					case DOWN:
+						playerMoved(0, +1, "down");
+						keyPressed = "DOWN!";
+						try {
+							sendInputToServer(keyPressed);
+						} catch (IOException e) {
+							throw new RuntimeException(e);
+						}
+						break;
+					case LEFT:
+						playerMoved(-1, 0, "left");
+						keyPressed = "LEFT!";
+						try {
+							sendInputToServer(keyPressed);
+						} catch (IOException e) {
+							throw new RuntimeException(e);
+						}
+						break;
+					case RIGHT:
+						playerMoved(+1, 0, "right");
+						keyPressed = "RIGHT!";
+						try {
+							sendInputToServer(keyPressed);
+						} catch (IOException e) {
+							throw new RuntimeException(e);
+						}
+						break;
+					default:
+						break;
 				}
 			});
-			
-            // Setting up standard players
-			
-			me = new Player("Johan",9,4,"up");
+
+			// Setting up standard players
+
+			me = new Player("Johan", 9, 4, "up");
 			players.add(me);
 			fields[9][4].setGraphic(new ImageView(hero_up));
 
-			Player harry = new Player("Harry",14,15,"up");
+			Player harry = new Player("Harry", 14, 15, "up");
 			players.add(harry);
 			fields[14][15].setGraphic(new ImageView(hero_up));
 
-			Player oskar = new Player("Oskar",12,4,"up");
+			Player oskar = new Player("Oskar", 12, 4, "up");
 			players.add(oskar);
 			fields[12][4].setGraphic(new ImageView(hero_up));
 
 			scoreList.setText(getScoreList());
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void playerMoved(int delta_x, int delta_y, String direction) {
 		me.direction = direction;
-		int x = me.getXpos(),y = me.getYpos();
+		int x = me.getXpos(), y = me.getYpos();
 
-		if (board[y+delta_y].charAt(x+delta_x)=='w') {
+		if (board[y + delta_y].charAt(x + delta_x) == 'w') {
 			me.addPoints(-1);
-		} 
-		else {
-			Player p = getPlayerAt(x+delta_x,y+delta_y);
-			if (p!=null) {
-              me.addPoints(10);
-              p.addPoints(-10);
+		} else {
+			Player p = getPlayerAt(x + delta_x, y + delta_y);
+			if (p != null) {
+				me.addPoints(10);
+				p.addPoints(-10);
 			} else {
 				me.addPoints(1);
-			
+
 				fields[x][y].setGraphic(new ImageView(image_floor));
-				x+=delta_x;
-				y+=delta_y;
+				x += delta_x;
+				y += delta_y;
 
 				if (direction.equals("right")) {
 					fields[x][y].setGraphic(new ImageView(hero_right));
-				};
+				}
+				;
 				if (direction.equals("left")) {
 					fields[x][y].setGraphic(new ImageView(hero_left));
-				};
+				}
+				;
 				if (direction.equals("up")) {
 					fields[x][y].setGraphic(new ImageView(hero_up));
-				};
+				}
+				;
 				if (direction.equals("down")) {
 					fields[x][y].setGraphic(new ImageView(hero_down));
-				};
+				}
+				;
 
 				me.setXpos(x);
 				me.setYpos(y);
@@ -194,20 +234,33 @@ public class GUI extends Application {
 	public String getScoreList() {
 		StringBuffer b = new StringBuffer(100);
 		for (Player p : players) {
-			b.append(p+"\r\n");
+			b.append(p + "\r\n");
 		}
 		return b.toString();
 	}
 
 	public Player getPlayerAt(int x, int y) {
 		for (Player p : players) {
-			if (p.getXpos()==x && p.getYpos()==y) {
+			if (p.getXpos() == x && p.getYpos() == y) {
 				return p;
 			}
 		}
 		return null;
 	}
 
+	public void sendInputToServer(String keyPressed) throws IOException {
+		InputStream stream = new ByteArrayInputStream(keyPressed.getBytes(StandardCharsets.UTF_8));
+		BufferedReader fromGame = new BufferedReader(new InputStreamReader(stream));
+		Socket clientSocket = new Socket("localhost", 6780);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		String sentence = fromGame.readLine();
+		outToServer.writeBytes(sentence + '\n');
+		String modifiedSentence = inFromServer.readLine();
+		System.out.println("FROM CLIENT: " + sentence);
+		System.out.println("FROM SERVER: " + modifiedSentence);
+		clientSocket.close();
+	}
 
 }
 
