e16c2f12d78a861ed944eea2370d12d5704a78f4
Daniel Møller <57629796+Danielm0402@users.noreply.github.com>
2023-03-09 09:07:01 +0100

    update

--- src/Server.java
+++ src/Server.java
@@ -15,8 +15,6 @@ public class Server {
 			ServerThread serverThread = new ServerThread(connectionSocket);
 			serverThread.start();
 			list.add(serverThread);
-			int index = list.size() - 1;
-			assignPlayers(index, serverThread);
 		}
 	}
 
@@ -24,9 +22,5 @@ public class Server {
 		for (ServerThread s : list){
 			s.skrivBytes(besked);
 		}
-	}
-
-	public static void assignPlayers(int index, ServerThread serverThread){
-		serverThread.setPlayer(GUI.players.get(index));
 	}
 }
--- src/ServerThread.java
+++ src/ServerThread.java
@@ -10,7 +10,6 @@ public class ServerThread extends Thread{
 	BufferedReader inFromClient;
 	DataOutputStream outToClient;
 	String clientSentence;
-	Player player;
 	
 	public ServerThread(Socket connSocket) {
 		this.connSocket = connSocket;
@@ -31,9 +30,5 @@ public class ServerThread extends Thread{
 	public void skrivBytes(String besked) throws IOException {
 		System.out.println(besked);
 		outToClient.writeBytes("ecco " + besked.toUpperCase() + '\n');
-	}
-
-	public void setPlayer(Player player) {
-		this.player = player;
 	}
 }
